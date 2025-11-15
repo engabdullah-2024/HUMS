76de09629d83be48539f0510dd78a60d8a5a4f2a
github.com/engabdullah-2024 <enga95311@gmail.com>
2025-11-15 14:21:02 +0300

    updated

--- app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-// src/app/api/auth/logout/route.ts
-import { NextRequest } from 'next/server';
-import { clearSessionCookie } from '@/lib/auth';
-
-export async function POST(_req: NextRequest) {
-  return clearSessionCookie();
-}
